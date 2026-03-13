0997b7082255102bf8250566ba804ba41706c623
Sumit Tech <chauhanamar3535@gmail.com>
2026-03-13 16:38:47 +0530

    fix: switch to static export for reliable Amplify deployment

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  output: 'export',
+  distDir: 'out',
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
